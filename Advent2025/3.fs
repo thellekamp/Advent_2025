e8fed675e463dcc1b5c68a8f582030889595222b
t.hellekamp@chipsoft.com <t.hellekamp@chipsoft.com>
2025-12-04 16:24:05 +0100

    day 3_1

---
+++ Advent2025/3.fs
@@ -0,0 +1,33 @@
+﻿module Advent2025_3
+
+open System
+open System.IO
+
+let lines = File.ReadAllLines(@"C:\Users\t.hellekamp\Documents\projects\fsharp\Advent2025\3\3.txt")
+
+let testItems = [| "987654321111111";"811111111111119";"234234234234278";"818181911112111" |]
+
+let latestNRemoved n (text:string) = text.Substring(0, text.Length - n)
+
+let latestOneRemoved = latestNRemoved 1
+
+let toInts (text:string) =
+    text.ToCharArray() |> Array.map(fun c -> Int32.Parse(c.ToString())) 
+
+let highestDigit (text:string) = 
+        text |> toInts |> Array.sortDescending |> Array.head  
+
+let highest (text:string) = 
+    let highestFirstDigit = 
+        text |> latestOneRemoved |> highestDigit
+    let highestSecondDigit =
+        text.Substring(text.IndexOf(highestFirstDigit.ToString()) + 1) |> highestDigit
+    highestFirstDigit * 10 + highestSecondDigit
+
+let advent2025_3_1 items =
+    items
+    |> Array.map highest
+    |> Array.sum
+
+testItems |> advent2025_3_1 |> printfn "test 1: %A"
+lines |> advent2025_3_1 |> printfn "test 1: %A"
---
+++ Advent2025/4.fs
@@ -0,0 +1,9 @@
+﻿module Advent2025_4
+
+open System
+open System.IO
+
+//let lines = File.ReadAllLines(@"C:\Users\t.hellekamp\Documents\projects\fsharp\Advent2025\1\1.txt")
+
+//let testItems = [| "L68";"L30";"R48";"L5";"R60";"L55";"L1";"L99";"R14";"L82" |]
+
